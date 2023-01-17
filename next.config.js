8e1be19e30a6eff3c1f0c27ce502c3e8ba17477b
Agung Wicaksono <gungcaksono.id@gmail.com>
2023-01-17 23:08:40 +0800

    init commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig

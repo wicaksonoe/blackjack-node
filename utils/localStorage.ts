0586ba167d3c0e249e2c702844dc476cfc2b5da0
Agung Wicaksono <gungcaksono.id@gmail.com>
2023-01-22 19:26:47 +0800

    fix: localstorage access code run on backend

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,13 +1,17 @@
 export function getLocalStorage<T>(key: string): T | undefined {
-  const storedValue = localStorage.getItem(key);
+  if (typeof window !== "undefined") {
+    const storedValue = localStorage.getItem(key);
 
-  if (storedValue) {
-    return JSON.parse(storedValue) as T;
-  } else {
-    return undefined;
+    if (storedValue) {
+      return JSON.parse(storedValue) as T;
+    } else {
+      return undefined;
+    }
   }
 }
 
 export function setLocalStorage(key: string, value: any) {
-  localStorage.setItem(key, JSON.stringify(value));
+  if (typeof window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
 }
